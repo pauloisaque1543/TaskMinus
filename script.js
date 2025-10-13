d42c685451515e03b071194afce15c66ae2dc27b
pauloisaque1543 <pauloisaquecontatocomercial@gmail.com>
2025-10-13 11:23:26 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const $ = (id) => document.getElementById(id);
-const $$ = (a, b, c) => a.addEventListener(b, c);
+class ListadeTarefas {
+    constructor() {
+        this.tarefas = [];
+    }
 
-const botaoDarkMode = $("dark-mode-button");
-$$(botaoDarkMode, "click", () => {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-});
+    adicionarTarefa(tarefa) {
+        this.tarefas.push(tarefa);
+    }
 
+    removerTarefa(id) {
+        this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+    }
 
+    listarTarefas() {
+        return this.tarefas;
+    }
+}
 
-
-
-console.log($("dark-mode-button"));
+const lista = new ListadeTarefas();
+lista.adicionarTarefa({ id: 1, descricao: 'Comprar leite' });
+lista.adicionarTarefa({ id: 2, descricao: 'Estudar JavaScript' });
+console.log(lista.listarTarefas());
+lista.removerTarefa(1);
+console.log(lista.listarTarefas());
