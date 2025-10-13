0216e4f4a6dc44f8809b5eda3bb66c6d4fdbd900
pauloisaque1543 <pauloisaquecontatocomercial@gmail.com>
2025-10-13 15:37:00 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const taskList = document.getElementById('task-list');
+const addTaskButton = document.getElementById('add-task-button');
+
+
 class ListadeTarefas {
     constructor() {
         this.tarefas = [];
@@ -5,16 +9,27 @@ class ListadeTarefas {
 
     adicionarTarefa(tarefa) {
         this.tarefas.push(tarefa);
+        taskList.innerHTML += `<li>${tarefa.descricao} <button onclick="removerTarefa(${tarefa.id})">Remover</button></li>`;
     }
 
     removerTarefa(id) {
         this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+        taskList.innerHTML = '';
+        this.tarefas.forEach(tarefa => {
+            taskList.innerHTML += `<li>${tarefa.descricao} <button onclick="removerTarefa(${tarefa.id})">Remover</button></li>`;
+        });
     }
 
     listarTarefas() {
         return this.tarefas;
     }
 }
+
+addTaskButton.addEventListener('click', () => {
+    const descricao = prompt('Descrição da tarefa:');
+   const id = Date.now();
+   lista.adicionarTarefa({ id, descricao });
+});
 
 const lista = new ListadeTarefas();
 lista.adicionarTarefa({ id: 1, descricao: 'Comprar leite' });
