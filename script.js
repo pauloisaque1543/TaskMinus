9df65be0085c0a7127622051b9f5d37a0014309d
pauloisaque1543 <pauloisaquecontatocomercial@gmail.com>
2025-10-13 10:45:27 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const $ = (id) => document.getElementById(id);
+const $$ = (a, b, c) => a.addEventListener(b, c);
+
+const botaoDarkMode = $("dark-mode-button");
+$$(botaoDarkMode, "click", () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
+
+
 
 
 console.log($("dark-mode-button"));
